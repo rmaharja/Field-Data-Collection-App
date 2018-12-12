06fab274775f69468e3b0d354af0ff55f9d90000
rmaharja@gmail.com <rmaharja@gmail.com>
2018-12-11 21:26:25 -0500

    Scaffold finish

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3001;
 
 
 app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }});
 
 
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/dataCollectionDB",
+  {
+    useNewUrlParser: true
+  }
+);
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
